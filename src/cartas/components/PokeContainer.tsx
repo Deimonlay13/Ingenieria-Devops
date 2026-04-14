4d978a6e862f994554276306d15793a07cfafcec
Deimonlay13 <di.pizarrom@duocuc.cl>
2026-04-13 22:35:06 -0400

    fix: agrega fallback mock cuando backend no responde

--- src/cartas/components/PokeContainer.tsx
+++ src/cartas/components/PokeContainer.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, type FC } from "react";
 import type { CartaPokemon } from "../interfaces/carta-pokemon.interface";
 import { PokeList } from "./PokeList";
+import { mockCartas } from "../mock/cartas.mock";
 
 export const PokeContainer: FC = () => {
   const [cards, setCards] = useState<CartaPokemon[]>([]);
@@ -9,7 +10,10 @@ useEffect(() => {
   fetch("http://localhost:8080/Producto/")
     .then(res => res.json())
     .then(data => setCards(data))
-    .catch(err => console.error("Error cargando cartas", err));
+    .catch(err => {
+      console.error("Error cargando cartas, usando mock local:", err);
+      setCards(mockCartas);
+    });
 }, []);
 
 
--- src/cartas/interfaces/carta-pokemon.interface.ts
+++ src/cartas/interfaces/carta-pokemon.interface.ts
@@ -1,6 +1,7 @@
 export interface CartaPokemon {
     id: string;
     nombre: string;
+    tipo?: string;
     img: string,
     precio: number;
     stock: number;
